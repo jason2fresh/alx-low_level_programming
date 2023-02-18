866f29fad9a1fcef4654c50e414d4a8330105194
jason2fresh <chikwesirichigozie5000@gmail.com>
2023-02-18 09:18:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  * Return: Always 1
-*/
+ */
 
 int main(void)
 {
-	write(2,"and that piece os art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece os art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
