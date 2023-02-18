1095e7a3851d40063b1f061d6a8871f6734571a1
jason2fresh <chikwesirichigozie5000@gmail.com>
2023-02-17 19:44:09 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with the put fuction
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
