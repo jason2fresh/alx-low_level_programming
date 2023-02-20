efebd00c25e860482b2eae217e9f9ae94d5b9848
jason2fresh <chikwesirichigozie5000@gmail.com>
2023-02-19 23:57:04 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int y = 97;
+
+	while (y <= 122)
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
